3e70911fbc9a590ff179963cb49d7a50ce60d775
Anita Chouhan <fe.dev2.ba2024@gmail.com>
2025-01-31 22:36:59 +0530

    updated code

--- pages/Navbar/Navbar.jsx
+++ pages/Navbar/Navbar.jsx
@@ -126,35 +126,42 @@ const Navbar = () => {
             </Link>
           </div>
           <div className="hidden md:flex justify-center items-center space-x-4">
+            {isLoggedIn ? (
+              <div>
+                <Link
+                  href="/dashboard"
+                  className="text-black hover:text-[#00b38d] px-3 py-2 rounded-md text-lg font-semibold"
+                >
+                  Dashboard
+                </Link>
+                <Link
+                  href="/dashboard/resumelist"
+                  className="text-black hover:text-[#00b38d] px-3 py-2 rounded-md text-lg font-semibold"
+                >
+                  My Resumes
+                </Link>
+                <Link
+                  href="/dashboard/cvletterlist"
+                  className="text-black hover:text-[#00b38d] px-3 py-2 rounded-md text-lg font-semibold"
+                >
+                  Cover Letter
+                </Link>
+              </div>
+            ) : (
+              <></>
+            )}
+
             <Link
-              href="/dashboard"
+              href="/navbarcontent"
               className="text-black hover:text-[#00b38d] px-3 py-2 rounded-md text-lg font-semibold"
             >
-              Dashboard
-            </Link>
-            <Link
-              href="/dashboard/resumelist"
-              className="text-black hover:text-[#00b38d] px-3 py-2 rounded-md text-lg font-semibold"
-            >
-              My Resumes
-            </Link>
-            <Link
-              href="/dashboard/cvletterlist"
-              className="text-black hover:text-[#00b38d] px-3 py-2 rounded-md text-lg font-semibold"
-            >
-              Cover Letter
+              AI Resume Builder
             </Link>
             <Link
               href="/dashboard/Jobs"
               className="text-black hover:text-[#00b38d] px-3 py-2 rounded-md text-lg font-semibold"
             >
               Jobs
-            </Link>
-            <Link
-              href="/navbarcontent"
-              className="text-black hover:text-[#00b38d] px-3 py-2 rounded-md text-lg font-semibold"
-            >
-              AI Resume Builder
             </Link>
             <Link
               href="https://blog.ciblijob.fr/"
@@ -363,24 +370,6 @@ const Navbar = () => {
           <div className="md:hidden absolute top-16 left-0 right-0 bg-white shadow-lg z-50">
             <div className="px-2 pt-2 pb-3 space-y-1">
               <Link
-                href="/dashboard"
-                className="text-black hover:text-[#00b38d] block px-3 py-2 rounded-md text-base font-semibold"
-              >
-                Dashboard
-              </Link>
-              <Link
-                href="/dashboard/resumelist"
-                className="text-black hover:text-[#00b38d] block px-3 py-2 rounded-md text-base font-semibold"
-              >
-                My Resumes
-              </Link>
-              <Link
-                href="/dashboard/cvletterlist"
-                className="text-black hover:text-[#00b38d] block px-3 py-2 rounded-md text-base font-semibold"
-              >
-                Cover Letter
-              </Link>
-              <Link
                 href="/dashboard/MyJobs"
                 className="text-black hover:text-[#00b38d] block px-3 py-2 rounded-md text-base font-semibold"
               >
@@ -409,8 +398,26 @@ const Navbar = () => {
               {isLoggedIn ? (
                 <>
                   <Link
+                    href="/dashboard"
+                    className="text-black hover:text-[#00b38d] block px-3 py-2 rounded-md text-base font-semibold"
+                  >
+                    Dashboard
+                  </Link>
+                  <Link
+                    href="/dashboard/resumelist"
+                    className="text-black hover:text-[#00b38d] block px-3 py-2 rounded-md text-base font-semibold"
+                  >
+                    My Resumes
+                  </Link>
+                  <Link
+                    href="/dashboard/cvletterlist"
+                    className="text-black hover:text-[#00b38d] block px-3 py-2 rounded-md text-base font-semibold"
+                  >
+                    Cover Letter
+                  </Link>
+                  <Link
                     href="/dashboard/page"
-                    className="block px-4 py-2 hover:bg-gray-200"
+                    className="text-black hover:text-[#00b38d] block px-3 py-2 rounded-md text-base font-semibold"
                   >
                     Profile
                   </Link>
